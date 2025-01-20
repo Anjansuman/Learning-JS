eedc286f6d09ba1a448b11daec711012d9ef28b6
Anjan <anjansuman80@gmail.com>
2025-01-21 00:05:20 +0530

    Maps in typescript

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -1,6 +1,17 @@
 "use strict";
-function sumOfAge(user1, user2) {
-    return user1.age + user2.age;
+function useProfile(user) {
+    console.log(`Name: ${user.name}, Email: ${user.email}`);
 }
-const added = sumOfAge({ name: "Anjan", age: 18 }, { name: "Suman", age: 20 });
-console.log(added);
+const data1 = {
+    name: "Anjan",
+    age: 18
+};
+// so, here I can change the age but not the name
+// data1.name = "Suman"; // this will through an error
+// but we can do this
+data1.age = 20;
+const data2 = {
+    name: "Anjan",
+    age: 18
+};
+// now this data can't be changed
