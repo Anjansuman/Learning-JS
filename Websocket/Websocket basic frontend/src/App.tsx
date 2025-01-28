92e2db87dabda2ea4f41ce4b8fe62da409b1bc9c
Anjan <anjansuman80@gmail.com>
2025-01-28 22:56:42 +0530

    a frontend talking to WebSocket-server.

--- Websocket/Websocket basic frontend/src/App.tsx
+++ Websocket/Websocket basic frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  let [ws, setWs] = useState();
+  const inputRef = useRef<HTMLInputElement>();
+
+  function sendMessage() {
+    const message = inputRef.current?.value;
+    ws.send(message);
+  }
+
+  // this is how we receive a message
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setWs(ws);
+    console.log("server connected");
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    }
+  }, [])
+
   return <div>
-    
+    <input ref={inputRef} type="text" placeholder='message' />
+    <button onClick={sendMessage}>send</button>
   </div>
 }
 
